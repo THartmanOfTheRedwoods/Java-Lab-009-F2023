b8786248d74b04eaf43c81f0d1acb18999a8aee3
Daniel Faulkner <dfaulkner707@gmail.com>
2023-10-28 23:00:53 -0700

    yep

--- src/Crack.java
+++ src/Crack.java
@@ -1,3 +1,4 @@
+// Daniel Faulkner, CIS 12, 10/28/23
 import org.apache.commons.codec.digest.Crypt;
 
 import java.io.FileInputStream;
@@ -16,21 +17,54 @@ public class Crack {
 
     public Crack(String shadowFile, String dictionary) throws FileNotFoundException {
         this.dictionary = dictionary;
-        this.users = Crack.parseShadow(shadowFile);
+        this.users = parseShadow(shadowFile);
     }
 
     public void crack() throws FileNotFoundException {
+        try (InputStream is = new FileInputStream(this.dictionary);
+             Scanner sc = new Scanner(is)) {
+            while (sc.hasNextLine()) {
+                String word = sc.nextLine();
+                for (User user : this.users) {
+                    String passHash = user.getPassHash();
+                    if (passHash.contains("$")) {
+                        String hash = Crypt.crypt(word, passHash);
+                        if (hash.equals(passHash)) {
+                            System.out.println("Found password " + word + " for user " + user.getUsername());
+                        }
+                    }
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public static int getLineCount(String path) {
         int lineCount = 0;
         try (Stream<String> stream = Files.lines(Path.of(path), StandardCharsets.UTF_8)) {
-            lineCount = (int)stream.count();
-        } catch(IOException ignored) {}
+            lineCount = (int) stream.count();
+        } catch (IOException ignored) {
+        }
         return lineCount;
     }
 
     public static User[] parseShadow(String shadowFile) throws FileNotFoundException {
+        User[] users = new User[getLineCount(shadowFile)];
+        try (InputStream is = new FileInputStream(shadowFile);
+             Scanner sc = new Scanner(is)) {
+            int index = 0;
+            while (sc.hasNextLine()) {
+                String line = sc.nextLine();
+                String[] parts = line.split(":");
+                User user = new User(parts[0], parts[1]);
+                users[index] = user;
+                index++;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return users;
     }
 
     public static void main(String[] args) throws FileNotFoundException {
@@ -42,5 +76,23 @@ public class Crack {
 
         Crack c = new Crack(shadowPath, dictPath);
         c.crack();
+    }
+}
+
+class User {
+    private String username;
+    private String passHash;
+
+    public User(String username, String passHash) {
+        this.username = username;
+        this.passHash = passHash;
+    }
+
+    public String getUsername() {
+        return this.username;
+    }
+
+    public String getPassHash() {
+        return this.passHash;
     }
 }
