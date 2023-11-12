53d5ea6f484cbe304343ab2fec36c5643142dd25
Atticus Blanco <atticusbrujx@gmail.com>
2023-11-11 17:35:06 -0800

    Part 2 >>>OPERATIONAL
    >>>MODIFICATIONS
    --Crack.java
      -METHOD splitLines; scanner object successfully wrapped a FileInputStream object; consider removing method
      -METHOD parseShadow; Scanner/FileInputStream wrap copied from splitLines method; while loop successfully splits username from passhash in each line.
    
    >>>DEFICITS
    --METHOD parseShadow; incomplete; pass the split usernames and passhashes to the User constructor within the while loop
    
    >>>BUGS
    --
    
    >>>THROWS
    --C:\Users\Gnomatic\IdeaProjects\Java-Lab-009\src\Crack.java
      Warning:(16, 27) Field can be converted to a local variable
      Warning:(17, 27) Private field 'fileContent' is never used
      Warning:(17, 27) Field 'fileContent' may be 'final'
      Warning:(26, 17) Method 'crack()' is never used
      Warning:(58, 27) Variable 'line' initializer '""' is redundant
      Warning:(62, 17) Variable 'i' is assigned but never accessed
      Warning:(63, 17) Variable 'j' is never used
      Warning:(97, 17) Variable 'sc' is never used
      Warning:(106, 29) Private method 'splitLines(java.lang.String)' is never used
      Warning:(136, 30) Variable 'fileInputStream' is never used
      Warning:(140, 11) Empty 'catch' block
      Warning:(144, 51) Exception 'java.io.FileNotFoundException' is never thrown in the method
      Warning:(152, 11) Empty 'catch' block

--- src/Crack.java
+++ src/Crack.java
@@ -1,9 +1,6 @@
 import org.apache.commons.codec.digest.Crypt;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -11,8 +8,15 @@ import java.util.Scanner;
 import java.util.stream.Stream;
 
 public class Crack {
+
+
     private final User[] users;
     private final String dictionary;
+    private static String shadowPath;
+    private static String dictPath;
+    private static String fileContent = "";
+
+
 
     public Crack(String shadowFile, String dictionary) throws FileNotFoundException {
         this.dictionary = dictionary;
@@ -20,6 +24,18 @@ public class Crack {
     }
 
     public void crack() throws FileNotFoundException {
+        Scanner s = new Scanner(new FileInputStream(this.dictionary));
+        while (s.hasNextInt()) {
+            String word = s.nextLine();
+            for (User user : users) {
+                if(user.getPassHash().startsWith("$")) {
+                    String hash = Crypt.crypt(word, user.getPassHash());
+                    System.out.println(hash);           //scaffold
+                    System.out.printf("Found password %s for user %s.", word, user.getUsername());
+                }
+
+            }
+        }
     }
 
     public static int getLineCount(String path) {
@@ -31,26 +47,109 @@ public class Crack {
     }
 
     public static User[] parseShadow(String shadowFile) throws FileNotFoundException {
+        User[] arrayUser = new User[getLineCount(shadowFile)];
+        System.out.printf("File as %d lines%n", arrayUser.length);            //scaffold
 
-//  Complete the body of the parseShadow method utilizing:
-//  the pre-complete method getLineCount to create a user array called Users
-//  imported class FileInputStream
-//  imported class Scanner
-//  a while loop that uses the above 2 imported classes to read the resources/shadow file line by line.
-//  the split method and the delimiter : to split each line into a string array (i.e. String[])
-//  use the first 2 elements of the split string array to create a new User(element1, element2)
-//  and finally store each new User into a User array (i.e. User[] users), and return the array.
+/*        String filePath = "resources/shadow";
+        File file = new File(filePath);
+        FileInputStream fis = new FileInputStream(file);*/
 
+        try {
+            String line = "";           //scaffold
+            String[] arraytest = new String[2];
+            System.out.println(arraytest.length);
+            Scanner s = new Scanner(new FileInputStream(shadowPath));
+            int i = 0;              //scaffold
+            int j = 0;              //scaffold
+            while (s.hasNextLine()) {
+                i++;                //scaffold
+
+
+//                System.out.printf("%d. %s%n", i, s.nextLine());     //scaffold
+                line = s.nextLine();            //scaffold
+                arraytest = line.split(":", 2);
+                System.out.printf("user: %s%npass: %s%n%n", arraytest[0], arraytest[1]);
+
+
+/*                do {                                       //scaffold
+                    System.out.println(arraytest[j]);       //scaffold
+                    j++;
+                } while (j < 1);                                //scaffold*/
+
+            }
+
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+//          imported class FileInputStream
+//          imported class Scanner
+//          a while loop that uses the above 2 imported classes to read the resources/shadow file line by line.
+//          the split method and the delimiter : to split each line into a string array (i.e. String[])
+
+
+//          use the first 2 elements of the split string array to create a new User(element1, element2)
+//          and finally store each new User into a User array (i.e. User[] users), and return the array.
+        return arrayUser;
+    }
+
+    public static void getPathAndDict(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Type the path to your shadow file: ");
+        shadowPath = /*sc.nextLine()*/ "resources/shadow";              //splint
+        System.out.println(shadowPath);                                 //scaffold
+        System.out.println("Type the path to your dictionary file: ");
+        dictPath = /*sc.nextLine()*/ "resources/englishSmall.dic";      //splint
+        System.out.println(dictPath);                                   //scaffold
+    }
+
+    private static String[] splitLines(String text) {
+        if (text == null || text.
+                isEmpty()) {
+            return new String[0]; // Return an empty array if the input is null or empty.
+        }
+
+        try {
+            Scanner s = new Scanner(new FileInputStream(shadowPath));
+            int i = 0;              //scaffold
+            while (s.hasNextLine()) {
+                i++;                //scaffold
+                System.out.printf("%d. %s%n", i, s.nextLine());
+            }
+
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+
+
+
+        return text.split("\\n"); // Split the text into an array of lines using newline as the delimiter.
+    }
+
+
+    public static void readShadowFile() throws IOException {
+        getPathAndDict();
+//        parseShadow(shadowPath);
+//        String[] fileLines = new String[getLineCount(shadowPath)];
+
+        try (FileInputStream fileInputStream = new FileInputStream(shadowPath)) {
+            parseShadow(shadowPath);
+//            fileLines = splitLines(new String(fileInputStream.readAllBytes(), StandardCharsets.UTF_8));
+
+        } catch (IOException e) {
+        }
     }
 
     public static void main(String[] args) throws FileNotFoundException {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Type the path to your shadow file: ");
-        String shadowPath = sc.nextLine();
-        System.out.print("Type the path to your dictionary file: ");
-        String dictPath = sc.nextLine();
-
+/*        getFilePath();
         Crack c = new Crack(shadowPath, dictPath);
-        c.crack();
+        c.crack();*/
+
+
+        try {
+            readShadowFile();
+        } catch (IOException e) {
+        }
     }
 }
