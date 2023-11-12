7ad68d8c1b2c2d2c087015b3a182df7b806ffc1f
Atticus Blanco <atticusbrujx@gmail.com>
2023-11-12 04:30:32 -0800

    Part 2 >>>OPERATIONAL
    >>>MODIFICATIONS
    --Crack.java
      -METHOD crack restructured while/for/if to for/if/do-while; Each username that has a password is accessed only once; nested do-while conditional successful.
      -Line 60 sout successfully prints correct output.
      -REMOVED pseudo
    
    >>>DEFICITS
    --
    
    >>>BUGS
    --
    
    >>>THROWS
    --C:\Users\Gnomatic\IdeaProjects\Java-Lab-009\src\Crack.java
      -Warning:(72, 64) Exception 'java.io.FileNotFoundException' is never thrown in the method
      -Warning:(78, 39) Variable 'arrNameAndPass' initializer 'new String[2]' is redundant

--- src/Crack.java
+++ src/Crack.java
@@ -23,40 +23,16 @@ public class Crack {
 
     public void crack() throws FileNotFoundException {
         Scanner s;
-
-//        int A = 0;              //scaffold
-//        int B = 0;              //scaffold
-//        int C = 0;              //scaffold
-
         for (User users : users) {
             s = new Scanner(new FileInputStream(this.dictionary));
-
-//            System.out.println("A" + A);        //scaffold
-//            A++;                                //scaffold
-
             if(users.getPassHash().startsWith("$")) {
-//                System.out.println("B" + B);        //scaffold
-//                B++;                                //scaffold
-
                 String hash;
                 String word;
-
-//                System.out.println(users.getPassHash().split(":",2)[0]);         //scaffold
-
                 do {
-//                    System.out.println("C" + C);        //scaffold
-//                    C++;                                //scaffold
-
                     word = s.nextLine();
-//                    System.out.println(word);       //scaffold
-
                     hash = Crypt.crypt(word, users.getPassHash());
-
-//                    System.out.println(hash);           //scaffold
                 } while (s.hasNextLine() && !(hash.equals(users.getPassHash().split(":",2)[0])));
-
-        System.out.printf("%nname: %s%npass: %s%n%n", users.getUsername(), word);
-
+        System.out.printf("%nname: %s%npass: %s%n", users.getUsername(), word);
             }
         }
     }
@@ -71,17 +47,13 @@ public class Crack {
 
     public static User[] parseShadow(String shadowFile) throws FileNotFoundException {
         User[] user = new User[getLineCount(shadowFile)];
-//        System.out.printf("File as %d lines%n", user.length);            //scaffold
-
         try {
-//            String line = "";           //scaffold
             String[] arrNameAndPass = new String[2];
             Scanner s = new Scanner(new FileInputStream(shadowPath));
             int i = 0;
             do {
                 arrNameAndPass = s.nextLine().split(":", 2);
                 user[i] = new User(arrNameAndPass[0], arrNameAndPass[1]);
-//                System.out.printf("user: %s%npass: %s%n%n", user[i].getUsername(), user[i].getPassHash());    //scaffold
                 i++;
             } while (s.hasNextLine());
         } catch (FileNotFoundException e) {
@@ -104,6 +76,5 @@ public class Crack {
             getPathAndDict();
             Crack c = new Crack(shadowPath, dictPath);
             c.crack();
-
     }
 }
